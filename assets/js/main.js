79f92a19ab031ec54575dc8148c8b25ba71b0751
Thabo <61136673+thabo11-hub@users.noreply.github.com>
2023-02-03 22:12:35 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Maxim - v4.9.0
-* Template URL: https://bootstrapmade.com/maxim-free-onepage-bootstrap-theme/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
